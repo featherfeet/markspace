d51575ae2a95271cbf3d221db9461790101d4ec3
Oliver Lok Trevor <featherfeet5436@gmail.com>
2020-01-12 11:54:13 -0800

    Moved starting/stopping the Spark server into separate threads so that the Swing GUI never locks up.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@
 
 import controllers.*;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.eclipse.jetty.server.Server;
 import storage.DatabaseStorage;
 import storage.PersistentStorage;
 
@@ -24,6 +25,7 @@ import java.util.Set;
 import javax.swing.*;
 
 import static spark.Spark.*;
+import static spark.Spark.stop;
 import static spark.debug.DebugScreen.*;
 
 public class Main extends JFrame implements ActionListener {
@@ -81,107 +83,12 @@ public class Main extends JFrame implements ActionListener {
         }
     }
 
-    private void startServer() {
-        // Show status and disable button.
-        start_server_button.setEnabled(false);
-        stop_server_button.setEnabled(true);
-        status_label.setText("<html><p style=\"width: 350px;\">Server starting...</p></html>");
-        // Configure Spark.
-        ipAddress("127.0.0.1");
-        port(4567); // Serve the application on port 4567.
-        if (!debug_mode) {
-            staticFiles.location("/static"); // All statically served resources (stylesheets, scripts, etc.) are stored here.
-        }
-        else {
-            staticFiles.externalLocation("/home/oliver/Projects/Web Projects/markspace/src/main/resources/static");
-            staticFiles.expireTime(1L);
-            enableDebugScreen();
-        }
-        // Configure web pages.
-        PersistentStorage persistentStorage = new DatabaseStorage(); // Create a DatabaseStorage object to interface the database. Only one should ever be instantiated; it is shared between all of the controllers.
-        // Instantiate objects for all of the controllers. Although the controllers' methods are static, they MUST be constructed in order to pass them a shared PersistentStorage object.
-        LoginController loginController = new LoginController(persistentStorage);
-        SignupController signupController = new SignupController(persistentStorage);
-        TestsController testsController = new TestsController(persistentStorage);
-        CreateNewTest1Controller createNewTest1Controller = new CreateNewTest1Controller(persistentStorage);
-        CreateNewTest2Controller createNewTest2Controller = new CreateNewTest2Controller(persistentStorage);
-        /* All of the controllers that do server-side rendering of PDFs share a cache of pre-loaded PDFRenderer objects,
-        indexed by which PDF they are associated with. */
-        Map<String, PDFRenderer> pdfRendererCache = new HashMap<>();
-        /* Since Jetty runs request handlers concurrently, a client requesting every page of a 50-page PDF to be
-        rendered would cause the server to create 50 of the same PDFRenderer object (since the PDFRenderer objects
-        only get placed into the cache after a request completes). To avoid this, a global Set of all PDFRenderer
-        objects in the process of being created is maintained here. While one request handler is creating a PDFRenderer,
-        it puts that PDFRenderer's key into this Set. This causes any other request handlers looking for the same
-        PDFRenderer to wait until it appears in the cache, thus reducing the creation of superfluous PDFRenderers. */
-        Set<String> renderersBeingCreated = new HashSet<>();
-        RenderTestController renderTestController = new RenderTestController(persistentStorage, pdfRendererCache, renderersBeingCreated);
-        TestController testController = new TestController(persistentStorage);
-        GetQuestionsController getQuestionsController = new GetQuestionsController(persistentStorage);
-        DeleteTestController deleteTestController = new DeleteTestController(persistentStorage);
-        SignoutController signoutController = new SignoutController(persistentStorage);
-        IndexController indexController = new IndexController(persistentStorage);
-        UploadStudentAnswersController uploadStudentAnswersController = new UploadStudentAnswersController(persistentStorage);
-        StudentAnswerFileController studentAnswerFileController = new StudentAnswerFileController(persistentStorage);
-        RenderQuestionController renderQuestionController = new RenderQuestionController(persistentStorage, pdfRendererCache, renderersBeingCreated);
-        GradeTestController gradeTestController = new GradeTestController(persistentStorage);
-        RenderStudentAnswerController renderStudentAnswerController = new RenderStudentAnswerController(persistentStorage);
-        GetStudentAnswersForTestController getStudentAnswersForTestController = new GetStudentAnswersForTestController(persistentStorage);
-        ScoreStudentAnswerController scoreStudentAnswerController = new ScoreStudentAnswerController(persistentStorage);
-        ViewStudentScoresController viewStudentScoresController = new ViewStudentScoresController(persistentStorage);
-        DownloadStudentScoresController downloadStudentScoresController = new DownloadStudentScoresController(persistentStorage);
-        // Configure which types of requests and what URLs correspond to each request handler.
-        get("/", indexController.serveIndexPageGet);
-        get("/index", indexController.serveIndexPageGet);
-        get("/login", loginController.serveLoginPageGet);
-        post("/login", loginController.serveLoginPagePost);
-        get("/signout", signoutController.serveSignoutPageGet);
-        get("/signup", signupController.serveSignupPageGet);
-        post("/signup", signupController.serveSignupPagePost);
-        get("/tests", testsController.serveTestsPageGet);
-        get("/create_new_test_1", createNewTest1Controller.serveCreateNewTest1PageGet);
-        post("/create_new_test_1", createNewTest1Controller.serveCreateNewTest1PagePost);
-        get("/create_new_test_2", createNewTest2Controller.serveCreateNewTest2PageGet);
-        post("/create_new_test_2", createNewTest2Controller.serveCreateNewTest2PagePost);
-        get("/render_test", renderTestController.serveRenderTestPageGet);
-        get("/test", testController.serveTestPageGet);
-        get("/get_questions", getQuestionsController.serveGetQuestionsPageGet);
-        get("/delete_test", deleteTestController.serveDeleteTestPageGet);
-        get("/upload_student_answers", uploadStudentAnswersController.serveUploadStudentAnswersPageGet);
-        post("/upload_student_answers", uploadStudentAnswersController.serveUploadStudentAnswersPagePost);
-        get("/student_answer_file", studentAnswerFileController.serveStudentAnswerFilePageGet);
-        get("/render_question", renderQuestionController.serveRenderQuestionPageGet);
-        get("/grade_test", gradeTestController.serveGradeTestPageGet);
-        get("/render_student_answer", renderStudentAnswerController.serveRenderStudentAnswerPageGet);
-        get("/get_student_answers_for_test", getStudentAnswersForTestController.serveGetStudentAnswersForTestPageGet);
-        post("/score_student_answer", scoreStudentAnswerController.serveScoreStudentAnswerPagePost);
-        get("/view_student_scores", viewStudentScoresController.serveViewStudentScoresPageGet);
-        get("/download_student_scores", downloadStudentScoresController.serveDownloadStudentScoresPageGet);
-        // Show status and open browser window.
-        status_label.setText("<html><p style=\"width: 350px;\">Server running! Browser should open automatically. If not, go to <a href=\"http://localhost:4567\">http://localhost:4567</a> in any browser.</p></html>");
-        try {
-            Desktop.getDesktop().browse(new URI("http://localhost:4567"));
-        }
-        catch (Exception e) {
-            System.out.println("Unable to open browser.");
-            e.printStackTrace();
-        }
-    }
-
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == start_server_button) {
-            System.out.println("Starting server...");
-            startServer();
-            System.out.println("Server started.");
+            new StartServerThread().start();
         }
         else if (e.getSource() == stop_server_button) {
-            System.out.println("Stopping server...");
-            status_label.setText("Stopping server...");
-            stop();
-            status_label.setText("Server stopped. Press start to restart it.");
-            start_server_button.setEnabled(true);
-            stop_server_button.setEnabled(false);
-            System.out.println("Server stopped.");
+            new StopServerThread().start();
         }
     }
 
@@ -191,5 +98,110 @@ public class Main extends JFrame implements ActionListener {
             Main main_obj = new Main();
             main_obj.setVisible(true);
         });
+    }
+
+    private class StartServerThread extends Thread {
+        public void run() {
+            // Print message.
+            System.out.println("Starting server...");
+            // Show status and disable button.
+            start_server_button.setEnabled(false);
+            stop_server_button.setEnabled(true);
+            status_label.setText("<html><p style=\"width: 350px;\">Server starting...</p></html>");
+            // Configure Spark.
+            ipAddress("127.0.0.1");
+            port(4567); // Serve the application on port 4567.
+            if (!debug_mode) {
+                staticFiles.location("/static"); // All statically served resources (stylesheets, scripts, etc.) are stored here.
+            }
+            else {
+                staticFiles.externalLocation("/home/oliver/Projects/Web Projects/markspace/src/main/resources/static");
+                staticFiles.expireTime(1L);
+                enableDebugScreen();
+            }
+            // Configure web pages.
+            PersistentStorage persistentStorage = new DatabaseStorage(); // Create a DatabaseStorage object to interface the database. Only one should ever be instantiated; it is shared between all of the controllers.
+            // Instantiate objects for all of the controllers. Although the controllers' methods are static, they MUST be constructed in order to pass them a shared PersistentStorage object.
+            LoginController loginController = new LoginController(persistentStorage);
+            SignupController signupController = new SignupController(persistentStorage);
+            TestsController testsController = new TestsController(persistentStorage);
+            CreateNewTest1Controller createNewTest1Controller = new CreateNewTest1Controller(persistentStorage);
+            CreateNewTest2Controller createNewTest2Controller = new CreateNewTest2Controller(persistentStorage);
+            /* All of the controllers that do server-side rendering of PDFs share a cache of pre-loaded PDFRenderer objects,
+            indexed by which PDF they are associated with. */
+            Map<String, PDFRenderer> pdfRendererCache = new HashMap<>();
+            /* Since Jetty runs request handlers concurrently, a client requesting every page of a 50-page PDF to be
+            rendered would cause the server to create 50 of the same PDFRenderer object (since the PDFRenderer objects
+            only get placed into the cache after a request completes). To avoid this, a global Set of all PDFRenderer
+            objects in the process of being created is maintained here. While one request handler is creating a PDFRenderer,
+            it puts that PDFRenderer's key into this Set. This causes any other request handlers looking for the same
+            PDFRenderer to wait until it appears in the cache, thus reducing the creation of superfluous PDFRenderers. */
+            Set<String> renderersBeingCreated = new HashSet<>();
+            RenderTestController renderTestController = new RenderTestController(persistentStorage, pdfRendererCache, renderersBeingCreated);
+            TestController testController = new TestController(persistentStorage);
+            GetQuestionsController getQuestionsController = new GetQuestionsController(persistentStorage);
+            DeleteTestController deleteTestController = new DeleteTestController(persistentStorage);
+            SignoutController signoutController = new SignoutController(persistentStorage);
+            IndexController indexController = new IndexController(persistentStorage);
+            UploadStudentAnswersController uploadStudentAnswersController = new UploadStudentAnswersController(persistentStorage);
+            StudentAnswerFileController studentAnswerFileController = new StudentAnswerFileController(persistentStorage);
+            RenderQuestionController renderQuestionController = new RenderQuestionController(persistentStorage, pdfRendererCache, renderersBeingCreated);
+            GradeTestController gradeTestController = new GradeTestController(persistentStorage);
+            RenderStudentAnswerController renderStudentAnswerController = new RenderStudentAnswerController(persistentStorage);
+            GetStudentAnswersForTestController getStudentAnswersForTestController = new GetStudentAnswersForTestController(persistentStorage);
+            ScoreStudentAnswerController scoreStudentAnswerController = new ScoreStudentAnswerController(persistentStorage);
+            ViewStudentScoresController viewStudentScoresController = new ViewStudentScoresController(persistentStorage);
+            DownloadStudentScoresController downloadStudentScoresController = new DownloadStudentScoresController(persistentStorage);
+            // Configure which types of requests and what URLs correspond to each request handler.
+            get("/", indexController.serveIndexPageGet);
+            get("/index", indexController.serveIndexPageGet);
+            get("/login", loginController.serveLoginPageGet);
+            post("/login", loginController.serveLoginPagePost);
+            get("/signout", signoutController.serveSignoutPageGet);
+            get("/signup", signupController.serveSignupPageGet);
+            post("/signup", signupController.serveSignupPagePost);
+            get("/tests", testsController.serveTestsPageGet);
+            get("/create_new_test_1", createNewTest1Controller.serveCreateNewTest1PageGet);
+            post("/create_new_test_1", createNewTest1Controller.serveCreateNewTest1PagePost);
+            get("/create_new_test_2", createNewTest2Controller.serveCreateNewTest2PageGet);
+            post("/create_new_test_2", createNewTest2Controller.serveCreateNewTest2PagePost);
+            get("/render_test", renderTestController.serveRenderTestPageGet);
+            get("/test", testController.serveTestPageGet);
+            get("/get_questions", getQuestionsController.serveGetQuestionsPageGet);
+            get("/delete_test", deleteTestController.serveDeleteTestPageGet);
+            get("/upload_student_answers", uploadStudentAnswersController.serveUploadStudentAnswersPageGet);
+            post("/upload_student_answers", uploadStudentAnswersController.serveUploadStudentAnswersPagePost);
+            get("/student_answer_file", studentAnswerFileController.serveStudentAnswerFilePageGet);
+            get("/render_question", renderQuestionController.serveRenderQuestionPageGet);
+            get("/grade_test", gradeTestController.serveGradeTestPageGet);
+            get("/render_student_answer", renderStudentAnswerController.serveRenderStudentAnswerPageGet);
+            get("/get_student_answers_for_test", getStudentAnswersForTestController.serveGetStudentAnswersForTestPageGet);
+            post("/score_student_answer", scoreStudentAnswerController.serveScoreStudentAnswerPagePost);
+            get("/view_student_scores", viewStudentScoresController.serveViewStudentScoresPageGet);
+            get("/download_student_scores", downloadStudentScoresController.serveDownloadStudentScoresPageGet);
+            // Show status and open browser window.
+            status_label.setText("<html><p style=\"width: 350px;\">Server running! Browser should open automatically. If not, go to <a href=\"http://localhost:4567\">http://localhost:4567</a> in any browser.</p></html>");
+            try {
+                Desktop.getDesktop().browse(new URI("http://localhost:4567"));
+            }
+            catch (Exception e) {
+                System.out.println("Unable to open browser.");
+                e.printStackTrace();
+            }
+            // Print message.
+            System.out.println("Server started.");
+        }
+    }
+
+    private class StopServerThread extends Thread {
+        public void run() {
+            System.out.println("Stopping server...");
+            status_label.setText("Stopping server...");
+            spark.Spark.stop();
+            status_label.setText("Server stopped. Press start to restart it.");
+            start_server_button.setEnabled(true);
+            stop_server_button.setEnabled(false);
+            System.out.println("Server stopped.");
+        }
     }
 }
