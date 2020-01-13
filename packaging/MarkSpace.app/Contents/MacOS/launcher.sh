5bfda5907ec7ba35caa5c01451acbba5a4b760f8
Oliver Lok Trevor <featherfeet5436@gmail.com>
2020-01-12 19:53:12 -0800

    Rebuilt with correct icon.

--- packaging/MarkSpace.app/Contents/MacOS/launcher.sh
+++ packaging/MarkSpace.app/Contents/MacOS/launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#../../PlugIns/Java.runtime/Contents/Home/bin/java -jar ../../Java/markspace*.jar
-/Users/oliver/markspace/packaging/MarkSpace.app/PlugIns/Java.runtime/Contents/Home/bin/java -jar /Users/oliver/markspace/packaging/MarkSpace.app/Java/markspace-1.0.jar
+echo "$(pwd)"
+/Users/oliver/markspace/packaging/MarkSpace.app/PlugIns/Java.runtime/Contents/Home/bin/java -Xdock:icon=/Users/oliver/markspace/packaging/MarkSpace.app/Contents/Resources/AppIcon.png -jar /Users/oliver/markspace/packaging/MarkSpace.app/Java/markspace-1.0.jar
