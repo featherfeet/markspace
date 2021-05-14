18161ab207cd16a8f245c76e9d3fb98fbcc2e5a5
Oliver Lok Trevor <featherfeet5436@gmail.com>
2021-05-14 11:53:46 -0700

    Rebuilt/repackaged with newer JSON Sanitizer library version.

--- packaging/MarkSpace.app/Contents/MacOS/launcher.sh
+++ packaging/MarkSpace.app/Contents/MacOS/launcher.sh
@@ -6,4 +6,4 @@ cd "$BASEDIR"
 cd ..
 cd ..
 
-./PlugIns/Java.runtime/Contents/Home/bin/java -Xdock:icon=./Contents/Resources/AppIcon.png -splash:./Contents/Resources/SplashScreen.png -jar ./Java/markspace-0.1.jar
+./PlugIns/Java.runtime/Contents/Home/bin/java -Xdock:icon=./Contents/Resources/AppIcon.png -splash:./Contents/Resources/SplashScreen.png -jar ./Java/markspace-0.0.2.jar
